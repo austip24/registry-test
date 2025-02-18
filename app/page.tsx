484a271957c3f811b6a2958cc532c763984a92d4
austip24 <austinpier24@gmail.com>
2025-02-17 22:00:32 -0700

    update registry.json

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         </p>
       </header>
       <main className="flex flex-col flex-1 gap-8">
-        {/* <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
           <div className="flex items-center justify-between">
             <h2 className="text-sm text-muted-foreground sm:pl-3">
               A simple hello world component
@@ -78,7 +78,7 @@ export default function Home() {
           <div className="flex items-center justify-center min-h-[400px] relative">
             <ExampleComboboxAccordion />
           </div>
-        </div> */}
+        </div>
 
         <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
           <div className="flex items-center justify-between">
